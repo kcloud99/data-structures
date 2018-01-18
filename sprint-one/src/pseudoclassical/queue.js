6f799e0186ba3e4d4699066bc3ba1aeb51b2e9f5
kcloud99 <kylemcleod1@gmail.com>
2018-01-18 11:18:15 -0800

    Implemented Stack and Queue functions using psuedoclassical methodology - passed tests

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,34 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.front = 0;
+  this.back = 0;
+  this.storage = {};
 };
+
+Queue.prototype.enqueue = function(val) {
+  this.storage[this.back] = val;
+  this.back++;
+};
+
+Queue.prototype.dequeue = function() {
+  if (this.back > this.front) {
+    var temp = this.storage[this.front];
+    delete this.storage[this.front];
+    this.front++;
+    return temp;
+  } else {
+    return;
+  }
+};
+
+Queue.prototype.size = function() {
+  return this.back - this.front;
+};
+
+var queue1 = new Queue();
+
+
+
 
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,29 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.stacksize = 0;
+  this.storage = {};
 };
 
 
+Stack.prototype.push = function(value) {
+  this.stacksize++;
+  this.storage[this.stacksize] = value;
+};
+
+Stack.prototype.pop = function() {
+  if (this.stacksize > 0){
+    var temp = this.storage[this.stacksize];
+    delete this.storage[this.stacksize];
+    this.stacksize--;
+    return temp;
+  } else {
+    return;
+  }
+};
+
+Stack.prototype.size = function() {
+  return this.stacksize;
+};
+
+var stack1 = new Stack();
