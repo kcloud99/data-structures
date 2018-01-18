7fa5ca2d42167f8c0803448f5b658378135d0aa4
kcloud99 <kylemcleod1@gmail.com>
2018-01-18 10:53:56 -0800

    Implemented Stack and Queue functions using prototypal methodology - passed tests

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,30 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+  someInstance.front = 0;
+  someInstance.back = 0;
+  someInstance.storage = {}; 
+
+  return someInstance;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(val) {
+  this.storage[this.back] = val;
+  this.back++;
+};
 
+queueMethods.dequeue = function() {
+  if (this.back > this.front) {
+    var temp = this.storage[this.front];
+    delete this.storage[this.front];
+    this.front++;
+    return temp;
+  }
+};
+
+queueMethods.size = function() {
+  return this.back - this.front;
+};
