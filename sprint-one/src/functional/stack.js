1eabca284b92193a78c9f1d0e594b1085367654c
kcloud99 <kylemcleod1@gmail.com>
2018-01-17 16:23:17 -0800

    Implemented stack function using functional programming paradigms and passed all tests

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,30 @@
 var Stack = function() {
   var someInstance = {};
-
+  var stackSize = 0;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    stackSize++;
+    storage[stackSize] = value;
+    
   };
 
+
   someInstance.pop = function() {
+    if (stackSize > 0) {
+      var temp = storage[stackSize];
+      delete storage[stackSize];
+      stackSize--;
+      return temp;
+    } else {
+      return;
+    }
   };
 
   someInstance.size = function() {
+    return stackSize; 
   };
 
   return someInstance;
