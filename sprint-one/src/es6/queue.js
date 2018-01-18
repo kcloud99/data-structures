a27e8e58c1430574f56340cdc8898d2e0e764566
kcloud99 <kylemcleod1@gmail.com>
2018-01-18 11:31:59 -0800

    Implemented Stack and Queue functions using ES6 classes with constructer paradigm - passed tests

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,31 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.front = 0;
+    this.back = 0;
+    this.storage = {};
+  }
+  
+  enqueue(value) {
+    this.storage[this.back] = value;
+    this.back++;
+  }
+
+  dequeue() {
+    if (this.back > this.front) {
+      var temp = this.storage[this.front];
+      delete this.storage[this.front];
+      this.front++;
+      return temp;
+    } else {
+      return;
+    }
+  } 
+
+  size() {
+    return this.back - this.front;
   }
 
 }
+
+var queue1 = new Queue();
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,30 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.stackSize = 0;
+    this.storage = {};
+  }
+
+  push(value) {
+    this.stackSize++;
+    this.storage[this.stackSize] = value;
+  }
+
+  pop() {
+    if (this.stackSize > 0) {
+      var temp = this.storage[this.stackSize];
+      delete this.storage[this.stackSize];
+      this.stackSize--;
+      return temp;
+    } else {
+      return;
+    }
+  }
+
+  size() {
+    return this.stackSize;
   }
 
 }
+
+var stack1 = new Stack();
