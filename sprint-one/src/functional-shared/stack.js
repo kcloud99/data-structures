d20a61d7d65c9855fb9205b0de7e936fee27d72e
kcloud99 <kylemcleod1@gmail.com>
2018-01-17 17:23:44 -0800

    Built scaffolding for Stack function using functional-shared paradigm

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,40 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+
+  someInstance.storage = {};
+
+  _.extend(someInstance, stackMethods);
+  return someInstance;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+  //this.stackSize++
+  //this.storage[] = value;
+};
 
+stackMethods.pop = function() {
+  var temp;
+};
+
+stackMethods.size = function() {
+  return;
+};
+
+
+
+
+/*
+//extend_.extend(destination, *sources) 
+Shallowly copy all of the properties in the source objects over to the destination object, 
+and return the destination object. Any nested objects or arrays will be copied by reference, 
+not duplicated. It's in-order, so the last source will override properties
+ of 
+the same name in previous arguments.
+
+_.extend({name: 'moe'}, {age: 50});
+=> {name: 'moe', age: 50}
+*/
