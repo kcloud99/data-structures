922bb3ab9401299b0e01fd7964b25d35b4d013f9
kcloud99 <kylemcleod1@gmail.com>
2018-01-18 10:17:21 -0800

    Implemented Stack and Queue functions using functional-shared methodology - passed tests

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,36 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {
+    first: 0,
+    last: 0,
+    storage: {}
+  };
+
+  _.extend(someInstance, queueMethods);
+
+  return someInstance;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+  this.storage[this.first] = value;
+  this.first++;
+};
+
+
+
+queueMethods.dequeue = function() {
+  if (this.first > this.last) {
+    var temp = this.storage[this.last];
+    delete this.storage[this.last];
+    this.last++;
+    return temp;
+  }
+};
+
+queueMethods.size = function() {
+  return this.first - this.last; 
+};
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,28 +1,41 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  var someInstance = {};
-
-  someInstance.storage = {};
+  
+  var someInstance = {
+    storage: {},
+    stackSize: 0     
+  };
 
   _.extend(someInstance, stackMethods);
   return someInstance;
 };
 
+
 var stackMethods = {};
 
 stackMethods.push = function(value) {
-  //this.stackSize++
-  //this.storage[] = value;
+  this.stackSize++;
+  this.storage[this.stackSize] = value;
 };
 
 stackMethods.pop = function() {
-  var temp;
+  if (this.stackSize > 0) {
+    var temp = this.storage[this.stackSize];
+    delete this.storage[this.stackSize];
+    this.stackSize--;
+    return temp;
+  } else {
+    return;
+  }
 };
 
 stackMethods.size = function() {
-  return;
+  return this.stackSize;
 };
+
+
+
 
 
 
