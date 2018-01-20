5e83eb5fc6f8a1841a499e08c6c415161482ab38
kcloud99 <kylemcleod1@gmail.com>
2018-01-20 10:48:19 -0800

    Implemented Graph and Tree structures and methods in the given styles and passed all tests

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,21 +3,76 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
 
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  // adds an array of value to tree
+  var someTree = Tree(value);
+  this.children.push(someTree);
 };
 
 treeMethods.contains = function(target) {
+  // look into object
+  // starting object will be
+
+  if (this.value === target) {
+    return true;
+  }
+  //debugger;
+  if (this.children.length > 0) {
+    for (var i = 0; i < this.children.length; i++) {
+      if (this.children[i].contains(target)) {
+        return true;
+      }
+      //return true;
+    }
+  }
+
+  return false;
 };
 
+// var familyTree = Tree('Joe');
+// var someOtherTree = Tree('kyle')
+// familyTree.contains('Andy') // true
+// someOtherTree.contains('kyle') // true
+
+// familyTree =
+// {
+//   value: 'Joe',
+//   children: [
+//     {
+//       value: 'Tom',
+//       children: [
+//         {
+//           value: 'Sam',
+//           children: []
+//         }
+//       ]
+//     },
+
+//     {
+//       value: 'Andy',
+//       children: []
+//     }
+//   ]
+// }
 
 
-/*
- * Complexity: What is the time complexity of the above functions?
- */
+
+
+
+
+
+
+
+
+
+
+
+
