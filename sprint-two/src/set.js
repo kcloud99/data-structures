43a8256a7bdf60dcb79885dfda0f60629a3b6710
kcloud99 <kylemcleod1@gmail.com>
2018-01-20 11:13:00 -0800

    Implemented basic version of Set and passed tests

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,19 +1,33 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = [];
   return set;
+
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (this._storage.indexOf(item) !== -1) {
+    return;
+  }
+  this._storage.push(item);
 };
 
+
 setPrototype.contains = function(item) {
+  if (this._storage.indexOf(item) >= 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 setPrototype.remove = function(item) {
+  var index = this._storage.indexOf(item);
+  this._storage.splice(index, 1);
 };
+
 
 /*
  * Complexity: What is the time complexity of the above functions?
